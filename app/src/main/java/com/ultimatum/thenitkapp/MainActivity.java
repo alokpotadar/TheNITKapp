8f0fadca5d1198fdd6326c47daac15064c3f100a
alokpotadar <alokpotadar@gmail.com>
2019-04-13 02:30:32 +0530

    First commit

---
+++ app/src/main/java/com/ultimatum/thenitkapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ultimatum.thenitkapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
